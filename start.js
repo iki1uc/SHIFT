41073e85e1b1a094ddd9624f277df047528cc4e0
iki1uc <entertrainer@web.de>
2026-05-26 18:02:52 +0200

    Update start.js

--- start.js
+++ start.js
@@ -1,1 +1,14 @@
 
+// SHIFT-System Instanz
+
+if (!localStorage.getItem("PREV")) localStorage.setItem("PREV", "index");
+if (!localStorage.getItem("NEXT")) localStorage.setItem("NEXT", "SHIFT");
+
+const script = document.createElement("script");
+script.src = "shift.js";
+document.head.appendChild(script);
+
+script.onload = () => {
+  const result = shiftCore("start");
+  shift_out(result);
+};
