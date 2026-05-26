355880b9ed02627ab3341685f8e9e0c6b5c2133c
iki1uc <entertrainer@web.de>
2026-05-26 18:41:16 +0200

    Update shift-core.js

--- shift-core.js
+++ shift-core.js
@@ -8,3 +8,21 @@ function shiftSinn(data) {
   };
 }
 
+function shiftCore(input) {
+  const schiene = shiftGetSchiene();
+  const eich = EICH();
+
+  const frame = {
+    cube: CUBE_ID,
+    role: ROLE,
+    in: input,
+    state: STATE,
+    schiene,
+    eich,
+    out: `SHIFT(${input})`
+  };
+
+  frame.sinn = shiftSinn(frame);
+
+  return frame;
+}
