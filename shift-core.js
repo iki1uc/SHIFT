3ac78836806a0f00489ba04d9dee7e9463e90f35
iki1uc <entertrainer@web.de>
2026-05-26 18:40:52 +0200

    Update shift-core.js

--- shift-core.js
+++ shift-core.js
@@ -1,1 +1,10 @@
+function shiftSinn(data) {
+  return {
+    essenz: data.out,
+    richtung: data.schiene.next,
+    zeit: data.eich.uhrzeit,
+    datum: data.eich.datum,
+    kern: `${data.cube} / ${data.role}`
+  };
+}
 
