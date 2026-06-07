574a042710f116050ffb693f6df74a2debb8d315
iki1uc <entertrainer@web.de>
2026-06-07 19:06:09 +0200

    Update shift.move.js

--- 4all/shift.move.js
+++ 4all/shift.move.js
@@ -1,1 +1,16 @@
 
+// SHIFT MOVE – Subsystem Aktivator
+export const ShiftMove = {
+    state: "EVO",   // neuer Standard
+    alt: "REV",     // alter Zustand
+
+    activate() {
+        console.log("SHIFT aktiviert:", this.state);
+        return this.state;
+    },
+
+    getAlt() {
+        console.log("SHIFT ALT:", this.alt);
+        return this.alt;
+    }
+};
