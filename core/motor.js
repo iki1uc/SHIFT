a01b993edeefc6a9055b70c841efb4face15819e
iki1uc <entertrainer@web.de>
2026-06-26 18:09:23 +0200

    Update motor.js

--- core/motor.js
+++ core/motor.js
@@ -1,1 +1,24 @@
+// IKI → 1 → UC Motor
+// Klarer Ablauf, klare Zeiten, klare Zustände
+
+const motor = [
+  { name: "IKI", time: 1000 },
+  { name: "1",   time: 1000 },
+  { name: "UC",  time: 1000 }
+];
+
+let pos = 0;
+
+function runMotor() {
+  const step = motor[pos];
+
+  console.log("Aktiv:", step.name);
+
+  setTimeout(() => {
+    pos = (pos + 1) % motor.length;
+    runMotor();
+  }, step.time);
+}
+
+runMotor();
 
